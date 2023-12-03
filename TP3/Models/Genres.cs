5785352f6b61967cc21fda6004c838e629c53250
jihen501 <jihene.mkhinini@insat.ucar.tn>
2023-12-03 21:31:02 +0100

    tt

--- TP3/Models/Genres.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Movie.Models
-{
-    public class Genres
-    {
-        public int Id { get; set; }
-        public string? GenreName { get; set; }
-        public ICollection<Movies>? Movies { get; set; }
-    }
-}
